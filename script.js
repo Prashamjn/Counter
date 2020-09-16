5816c4338a9818c6ac394912d880daf0d5a9b9bf
prashamjn <prasham2306@gmail.com>
2020-09-16 16:17:48 +0530

    first commit

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 function counter(){
+    var counter = 0;
     var counter = document.getElementById('time');
-    var c = new Date().getSeconds();
-
-    counter.innerHTML = c;
+    for (let i = 0; i < 101; i++) {
+        counter = i;
+        setInterval(+1);
+    }
 }
+var c = new Date().getSeconds();
 
+counter.innerHTML = c;
 var interval = setInterval(counter, 1000)
